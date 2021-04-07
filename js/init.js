d85980515f71d27580e62e5c829abeb9e73f138f
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-04-07 12:13:42 -0500

    Primer commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDj6AG3OACpl5v9s8tFLLm8TiVKJbBnaj4",
+  authDomain: "aypmmash.firebaseapp.com",
+  databaseURL: "https://aypmmash.firebaseio.com",
+  projectId: "aypmmash",
+  storageBucket: "aypmmash.appspot.com",
+  messagingSenderId: "777885248345",
+  appId: "1:777885248345:web:45945301cb79ea64253a48"
+});
