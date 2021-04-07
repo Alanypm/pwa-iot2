7f2a51be9279d660573b7940d6cd7888c6c44ec4
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-04-07 12:34:34 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDj6AG3OACpl5v9s8tFLLm8TiVKJbBnaj4",
-  authDomain: "aypmmash.firebaseapp.com",
-  databaseURL: "https://aypmmash.firebaseio.com",
-  projectId: "aypmmash",
-  storageBucket: "aypmmash.appspot.com",
-  messagingSenderId: "777885248345",
-  appId: "1:777885248345:web:45945301cb79ea64253a48"
+  apiKey: "AIzaSyDL71Mq-MsW2d8Y8zdEZJjoug_KJc90gwI",
+  authDomain: "aypmbd.firebaseapp.com",
+  databaseURL: "https://aypmbd.firebaseio.com",
+  projectId: "aypmbd",
+  storageBucket: "aypmbd.appspot.com",
+  messagingSenderId: "71455841334",
+  appId: "1:71455841334:web:873522a51aa7a4b4a60c97"
 });
